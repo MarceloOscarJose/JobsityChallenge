f0cbd940c8fd4c692a6f627cd06ed539e5c4292d
Marcelo Oscar José <marcelo.oscar.jose@gmail.com>
2019-04-24 06:22:16 -0300

    Some general classes and groups

--- JobsityChallenge/Main/MainViewController.swift
+++ JobsityChallenge/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  JobsityChallenge
 //
 //  Created by Marcelo José on 23/04/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
